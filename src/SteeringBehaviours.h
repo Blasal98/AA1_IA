0505eb1a8bdf99d280ae0fc9b51fbe9059a19c77
Eloi García Sánchez <eloi.garcia@enti.cat>
2020-10-20 09:44:38 +0200

    Flocking mig mig hi ha coses que no aconsegueixo arreglar
    no aconsegueixo fer bé el bucle i no se si falta include per fer el normalize

--- src/SteeringBehaviours.h
+++ src/SteeringBehaviours.h
@@ -32,6 +32,67 @@ namespace SteeringBehaviours {
 		}
 		return doesItIntersect;
 	}
+	
+	void Flocking(Agent *agent, std::vector<Agent> agents, int agent_index, float dtime)
+	{
+		Vector2D separationDir;
+		Vector2D cohesionDir;
+		Vector2D alignmentDir;
+		CalculateSeparationDirection(agents, agent_index, separationDir);
+		CalculateCohesionDirection(agents, agent_index, cohesionDir);
+		CalculateAlignmentDirection(agents, agent_index, alignmentDir);
+		Vector2D FlockingForce = separationDir * K_SEPARATION_FORCE + cohesionDir * K_COHESION_FORCE + alignmentDir * K_ALIGNMENT_FORCE;
+		agent->addForce(FlockingForce);
+	}
 
+	void CalculateSeparationDirection(std::vector<Agent> agents, int agent_index, Vector2D separationDir)
+	{
+		int neighbourCount = agent_index;
+		Vector2D separationVec;
+		//foreach agent in array no se com posarlo
+		for each() 
+		{
+			if (distance(agents[agent_index].getPosition, position) < NEIGHTBOUR_RADIUS)
+			{
+				separationVec += (position - agents[agent_index]);
+				neighbourCount++;
+			}
+		}
+		separationVec /= neighbourCount;
+		separationDir = normalize(separationVec);
+	}
 
+	void CalculateCohesionDirection(std::vector<Agent> agents, int agent_index, Vector2D cohesionDir)
+	{
+		int neighbourCount = agent_index;
+		Vector2D cohesionVec;
+		//foreach agent in array no se com posarlo
+		for each()
+		{
+			if (distance(agents[agent_index].getPosition, position) < NEIGHTBOUR_RADIUS)
+			{
+				cohesionVec += (position - agents[agent_index].getPosition);
+				neighbourCount++;
+			}
+		}
+		cohesionVec /= neighbourCount;
+		cohesionDir = normalize(cohesionVec);
+	}
+
+	void CalculateAlignmentDirection(std::vector<Agent> agents, int agent_index, Vector2D alignmentDir)
+	{
+		int neighbourCount = agent_index;
+		Vector2D averageVelocity;
+		//foreach agent in array no se com posarlo
+		for each()
+		{
+			if (distance(agents[agent_index].getPosition, position) < NEIGHTBOUR_RADIUS)
+			{
+				averageVelocity += agents[agent_index].getVelocity;
+				neighbourCount++;
+			}
+		}
+		averageVelocity /= neighbourCount;
+		alignmentDir = normalize(averageVelocity);
+	}
 }
